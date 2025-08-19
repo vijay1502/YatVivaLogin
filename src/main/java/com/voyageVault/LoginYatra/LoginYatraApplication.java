ca4cf31ee4502ee818412e85b47313c6e8ab3806
vijay1502 <pruthvikrishna97@gmail.com>
2025-08-19 22:45:37 +0530

    First Commit

---
+++ src/main/java/com/voyageVault/LoginYatra/LoginYatraApplication.java
@@ -0,0 +1,13 @@
+package com.voyageVault.LoginYatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginYatraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginYatraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voyageVault/LoginYatra/LoginYatraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyageVault.LoginYatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginYatraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
